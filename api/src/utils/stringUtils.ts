abdf847f4e345f28c887f2242bce30b2210ded47
NKoelblen <hello@onoko.dev>
2026-04-28 13:30:36 +0200

    feat: update sanitizeWysiwyg function to allow additional heading tags and modify allowed attributes for improved HTML sanitization

--- api/src/utils/stringUtils.ts
+++ api/src/utils/stringUtils.ts
@@ -26,10 +26,17 @@ export function slugify(text: string): string {
  */
 export function sanitizeWysiwyg(html: string): string {
   return sanitizeHtml(html, {
-    allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img", "h1", "h2"]),
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat([
+      "h1",
+      "h2",
+      "h3",
+      "h4",
+      "h5",
+      "h6",
+    ]),
     allowedAttributes: {
       ...sanitizeHtml.defaults.allowedAttributes,
-      img: ["src", "alt", "title", "width", "height"],
+      "*": ["class"],
     },
     allowedSchemes: ["data", "http", "https"],
   });
